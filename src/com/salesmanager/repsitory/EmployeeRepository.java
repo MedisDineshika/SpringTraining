d1172bc9ffe11cebd7d1d7d2c8fdcc7bea3ef9bc
WDM <52590387+MedisDineshika@users.noreply.github.com>
2020-01-24 06:22:46 +0530

    Create EmployeeRepository.java

---
+++ src/com/salesmanager/repsitory/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.salesmanager.repsitory;
+
+import java.util.List;
+
+import com.salesmanager.model.Employee;
+
+public interface EmployeeRepository {
+
+	List<Employee> getAllEmployees();
+
+}
