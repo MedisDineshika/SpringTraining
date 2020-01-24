9d07e3b1e2d269923d2d48d5d60622989d3d2147
WDM <52590387+MedisDineshika@users.noreply.github.com>
2020-01-24 06:24:38 +0530

    Create EmployeeService.java

---
+++ src/com/salesmanager/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.salesmanager.service;
+
+import java.util.List;
+
+import com.salesmanager.model.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> getAllEmployees();
+}
